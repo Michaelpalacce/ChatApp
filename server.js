2f72f739dc1548fee8d6159b5666f0b97c02999c
Michaelpalacce <SaveTheFuture123>
2020-02-08 01:10:37 +0200

    [master] Progress commit

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+'use strict';
+
 const { Server, Loggur }	= require( 'event_request' );
+const ejs					= require( 'ejs' );
+const socketIO				= require( 'socket.io' );
+const path					= require( 'path' );
+const http					= require( 'http' );
 
 /**
  * @brief	Instantiate the server
  */
-const server	= Server();
+const app					= Server();
 
-// Add a new Route
-server.get( '/', ( event ) => {
-	event.send( '<h1>Hello World!</h1>' );
+app.apply( 'er_static_resources', { paths : ['public', 'favicon.ico'] } );
+app.apply( 'er_templating_engine', { templateDir : path.join( __dirname, './public' ), engine : ejs } );
+
+app.get( '/',( event )=>{
+	event.render( 'index' );
 });
 
-server.start( ()=>{
-	Loggur.log( 'Server started' );
-});
+Server.listen( process.env.PORT || 80, '0.0.0.0', ()=>{
+	Loggur.log( 'Server is UN' )
+} );
