f2e05806225883d6eaffa94ecb629bd259fcd5fe
Michaelpalacce <SaveTheFuture123>
2020-02-08 11:09:14 +0200

    Implemented Socket IO connect and disconnect on client and server

--- server.js
+++ server.js
@@ -10,14 +10,27 @@ const http					= require( 'http' );
  * @brief	Instantiate the server
  */
 const app					= Server();
+const server				= http.createServer( Server.attach() );
+const io					= socketIO( server );
 
-app.apply( 'er_static_resources', { paths : ['public', 'favicon.ico'] } );
+app.apply( 'er_static_resources', { paths : ['public'] } );
 app.apply( 'er_templating_engine', { templateDir : path.join( __dirname, './public' ), engine : ejs } );
 
 app.get( '/',( event )=>{
 	event.render( 'index' );
 });
 
-Server.listen( process.env.PORT || 80, '0.0.0.0', ()=>{
-	Loggur.log( 'Server is UN' )
+io.on( 'connection', ( socket )=>{
+	Loggur.log( 'New user connected' );
+
+	socket.on( 'disconnect', ()=>{
+		console.log( 'User Disconnected' )
+	} );
+} );
+
+
+
+server.listen( process.env.PORT || 3000, '0.0.0.0', ()=>{
+	Loggur.log( 'Server is UN' );
+	Loggur.log( process.env.PORT || 3000 )
 } );
