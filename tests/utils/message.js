8fa4e4d1d31b48c90628edf889a4de1bc96f923e
Michaelpalacce <SaveTheFuture123>
2020-02-08 15:25:59 +0200

    Added generateLocationMessage tests

--- tests/utils/message.js
+++ tests/utils/message.js
@@ -1,7 +1,10 @@
 const { Development }	= require( 'event_request' );
 const { test, assert }	= Development.Testing;
 
-const { generateMessage }	= require( '../../src/utils/message' );
+const {
+	generateMessage,
+	generateLocationMessage
+}	= require( '../../src/utils/message' );
 
 test({
 	message: 'generateMessage should generate correct message object',
@@ -12,6 +15,20 @@ test({
 
 		assert.equal( message.text, text );
 		assert.equal( message.from, from );
+
+		done();
+	}
+});
+test({
+	message: 'generateLocationMessage should generate correct message object',
+	test: ( done )=>{
+		const from		= 'John Smith';
+		const longitude	= 100;
+		const latitude	= 100;
+		const message	= generateLocationMessage( from, latitude, longitude );
+
+		assert.equal( message.from, from );
+		assert.equal( message.url, `https://www.google.com/maps?q=${latitude},${longitude}` );
 
 		done();
 	}
